3cac47dd86c4c95d746769805099f011962c3ee0
Babak Aliyev <114646196+babakaliyev@users.noreply.github.com>
2023-07-04 22:37:51 +0400

    done

--- assignment_2_starter/task_03/app.js
+++ assignment_2_starter/task_03/app.js
@@ -1,178 +1,181 @@
 let gl, program;
 let vertexCount = 36;
-let modelViewMatrix, projectionMatrix;
-let eye = [0, 0, 0.1];
+let modelViewMatrix;
+let projectionMatrix;
+let eye = [2, 2, 2]; 
+let up=[0,1,0];
 let at = [0, 0, 0];
-let up = [0, 1, 0];
-let fovy = 60; // Field of view in degrees
-let aspect = 1; // Aspect ratio of the canvas
-let near = 0.1; // Near clipping plane
-let far = 10.0; // Far clipping plane
+let left = -2, right = 2, bottom = -2, ytop = 2, near = -10, far = 10;
+let fovy = 45;
 
 onload = () => {
-    let canvas = document.getElementById("webgl-canvas");
+  let canvas = document.getElementById("webgl-canvas");
 
-    gl = WebGLUtils.setupWebGL(canvas);
-    if (!gl) {
-        alert('No webgl for you');
-        return;
-    }
+  gl = WebGLUtils.setupWebGL(canvas);
+  if (!gl) {
+    alert('No webgl for you');
+    return;
+  }
 
-    program = initShaders(gl, 'vertex-shader', 'fragment-shader');
-    gl.useProgram(program);
+  program = initShaders(gl, 'vertex-shader', 'fragment-shader');
+  gl.useProgram(program);
 
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-    gl.enable(gl.DEPTH_TEST);
+  gl.enable(gl.DEPTH_TEST);
 
-    gl.clearColor(0, 0, 0, 0.5);
+  gl.clearColor(0, 0, 0, 0.5);
 
-    let vertices = [
-        -1, -1, 1,
-        -1, 1, 1,
-        1, 1, 1,
-        1, -1, 1,
-        -1, -1, -1,
-        -1, 1, -1,
-        1, 1, -1,
-        1, -1, -1,
-    ];
+  let vertices = [
+    -1, -1, 1,
+    -1, 1, 1,
+    1, 1, 1,
+    1, -1, 1,
+    -1, -1, -1,
+    -1, 1, -1,
+    1, 1, -1,
+    1, -1, -1,
+  ];
 
-    let indices = [
-        0, 3, 1,
-        1, 3, 2,
-        4, 7, 5,
-        5, 7, 6,
-        3, 7, 2,
-        2, 7, 6,
-        4, 0, 5,
-        5, 0, 1,
-        1, 2, 5,
-        5, 2, 6,
-        0, 3, 4,
-        4, 3, 7,
-    ];
+  let indices = [
+    0, 3, 1,
+    1, 3, 2,
+    4, 7, 5,
+    5, 7, 6,
+    3, 7, 2,
+    2, 7, 6,
+    4, 0, 5,
+    5, 0, 1,
+    1, 2, 5,
+    5, 2, 6,
+    0, 3, 4,
+    4, 3, 7,
+  ];
 
-    let colors = [
-        0, 0, 0,
-        0, 0, 1,
-        0, 1, 0,
-        0, 1, 1,
-        1, 0, 0,
-        1, 0, 1,
-        1, 1, 0,
-        1, 1, 1,
-    ];
+  let colors = [
+    0, 0, 0,
+    0, 0, 1,
+    0, 1, 0,
+    0, 1, 1,
+    1, 0, 0,
+    1, 0, 1,
+    1, 1, 0,
+    1, 1, 1,
+  ];
 
-    vertices = scale(0.5, vertices);
+  let vBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
-    let vBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  let vPosition = gl.getAttribLocation(program, 'vPosition');
+  gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vPosition);
 
-    let vPosition = gl.getAttribLocation(program, 'vPosition');
-    gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
+  let iBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
 
-    let iBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
+  let cBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
 
-    let cBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+  let vColor = gl.getAttribLocation(program, 'vColor');
+  gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vColor);
 
-    let vColor = gl.getAttribLocation(program, 'vColor');
-    gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vColor);
+  modelViewMatrix = gl.getUniformLocation(program, 'modelViewMatrix');
+  projectionMatrix = gl.getUniformLocation(program, 'projectionMatrix');
 
-    modelViewMatrix = gl.getUniformLocation(program, 'modelViewMatrix');
-    projectionMatrix = gl.getUniformLocation(program, 'projectionMatrix');
+  document.addEventListener('keydown', handleKeyDown);
 
-    document.addEventListener('keydown', handleKeyDown);
-
-    render();
+  render();
 };
-//  these keyboard events will trigger the camera
+// These keyboard events will trigger the camera
 function handleKeyDown(event) {
-    switch (event.key) {
-        // Top-side view
-        case 't':
-        case 'T':
-            eye = [0, 1, 0.1]; 
-            break;
-            // Left-side view
-        case 'l':
-        case 'L':
-            eye = [-1, 0, 0.1]; 
-            break;
-            // Front-side view
-        case 'f':
-        case 'F':
-            eye = [0, 0, 0.1]; 
-            break;
-            // clockwise by 10 degrees
-        case 'd':
-        case 'D':
-            rotateCamera(10); 
-            break;
-            //counter-clockwise by 10 degrees
-        case 'a':
-        case 'A':
-            rotateCamera(-10); 
-            break;
-            // zoom in
-        case 'w':
-        case 'W':
-            zoomIn();
-            break;
-            // zoom out
-        case 's':
-        case 'S':
-            zoomOut();
-            break;
-    }
+  switch (event.key) {
+    // Clockwise by 10 degrees
+    case 'd':
+    case 'D':
+      rotateCamera(10); 
+      break;
+    // Counter-clockwise by -10 degrees
+    case 'a':
+    case 'A':
+      rotateCamera(-10); 
+      break;
+    // Isometric view
+    case 'i':
+    case 'I':
+      eye = [2, 2, 2]; 
+      up=[0,1,0];
+      break;
+    // Zoom in
+    case 'w':
+    case 'W':
+      zoomIn(); 
+      break;
+    // Zoom out
+    case 's':
+    case 'S':
+      zoomOut(); 
+      break;
+  }
 
-    render();
+  render();
 }
-
+function zoomIn(){
+  eye[0] *= 0.5;
+  eye[1] *= 0.5;
+  eye[2] *= 0.5;
+}
+function zoomOut(){
+  eye[0] *= 1.1;
+  eye[1] *= 1.1;
+  eye[2] *= 1.1;
+}
 function rotateCamera(theta) {
-    // here we converted theta to radians
-    let radians = theta * Math.PI / 180;
-  
-    //  a rotation matrix
-    let rotationMatrix = mat3(
+  let radians = theta * Math.PI / 180;
+  let rotationMatrix;
+
+  if (eye[0] === 0 && eye[1] === 0 && eye[2] === 0.1) {  // Front-side view
+    rotationMatrix = mat3(
       Math.cos(radians), -Math.sin(radians), 0,
       Math.sin(radians), Math.cos(radians), 0,
       0, 0, 1
     );
-  
-    // rotating up vector
-    up = vec3(
-      rotationMatrix[0][0] * up[0] + rotationMatrix[0][1] * up[1] + rotationMatrix[0][2] * up[2],
-      rotationMatrix[1][0] * up[0] + rotationMatrix[1][1] * up[1] + rotationMatrix[1][2] * up[2],
-      rotationMatrix[2][0] * up[0] + rotationMatrix[2][1] * up[1] + rotationMatrix[2][2] * up[2]
+  } else if (eye[0] === -1 && eye[1] === 0 && eye[2] === 0) {  // Left-side view
+    rotationMatrix = mat3(
+      Math.cos(radians), 0, Math.sin(radians),
+      0, 1, 0,
+      -Math.sin(radians), 0, Math.cos(radians)
+    );
+  } else {
+    rotationMatrix = mat3(
+      Math.cos(radians), -Math.sin(radians), 0,
+      Math.sin(radians), Math.cos(radians), 0,
+      0, 0, 1
     );
   }
 
-function zoomIn() {
-    fovy -= 5; // Decrease the field of view
-    if (fovy < 1) fovy = 1; // Limit the minimum field of view
+  up = vec3(
+    rotationMatrix[0][0] * up[0] + rotationMatrix[0][1] * up[1] + rotationMatrix[0][2] * up[2],
+    rotationMatrix[1][0] * up[0] + rotationMatrix[1][1] * up[1] + rotationMatrix[1][2] * up[2],
+    rotationMatrix[2][0] * up[0] + rotationMatrix[2][1] * up[1] + rotationMatrix[2][2] * up[2]
+  );
 }
 
-function zoomOut() {
-    fovy += 5; // Increase the field of view
-    if (fovy > 179) fovy = 179; // Limit the maximum field of view
-}
+
 
 function render() {
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  
+  let mvm = lookAt(eye, at, up);
+  let proj = perspective(fovy, gl.canvas.width / gl.canvas.height, near, far);
 
-    let mvm = lookAt(eye, at, up);
-    gl.uniformMatrix4fv(modelViewMatrix, false, flatten(mvm));
+  gl.uniformMatrix4fv(modelViewMatrix, false, flatten(mvm));
+  gl.uniformMatrix4fv(projectionMatrix, false, flatten(proj));
 
-    let proj = perspective(fovy, aspect, near, far);
-    gl.uniformMatrix4fv(projectionMatrix, false, flatten(proj));
+  gl.drawElements(gl.TRIANGLES, vertexCount, gl.UNSIGNED_BYTE, 0);
 
-    gl.drawElements(gl.TRIANGLES, vertexCount, gl.UNSIGNED_BYTE, 0);
+  requestAnimationFrame(render);
 }
